5c6b3b24efc5a0a067524cda7ec76967fbe945e7
nurzainpradana <pradananurzain@gmail.com>
2021-08-05 13:22:04 +0700

    try to create bottom sheet

---
+++ app/src/test/java/com/nurzainpradana/androidfundamental/submission2moviecataloguetesting/ui/detailmovie/DetailMovieVMTest.kt
@@ -0,0 +1,12 @@
+package com.nurzainpradana.androidfundamental.submission2moviecataloguetesting.ui.detailmovie
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DetailMovieVMTest {
+
+    @Test
+    fun getDetailMovie() {
+    }
+}
